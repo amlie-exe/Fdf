cf6a9bf854ab6aaf0742186c03979ed607d7062f
amlie-exe <amhan@student.42.fr>
2025-08-01 18:06:50 +0200

    parsing functions

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: amhan <amhan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 13:42:29 by amhan             #+#    #+#             */
-/*   Updated: 2025/07/30 17:53:47 by amhan            ###   ########.fr       */
+/*   Updated: 2025/08/01 15:53:03 by amhan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,16 @@
 # include <math.h>
 # include <stdio.h>
 
-int	main(void);
+typedef struct s_map
+{
+	int	width_x;
+	int	height_y;
+	int	**data_z;
+}		t_map;
+
+t_map	*parse_map(char *filename);
+int		count_lines_y(char *filename);
+int		count_width_x(char *filename);
+void	free_tab(char **tab);
 
 #endif
